f9c6e3c95860627edd4882db9e120af125df916f
Alex Noutash <alnoutash@hotmail.com>
2025-10-20 20:43:22 -0400

    first component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SideMenu} from './side-menu/side-menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideMenu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

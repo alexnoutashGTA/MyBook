e59b8aefa2ee4415d8ed99cfb2f0f54c6ef35545
Alex Noutash <alnoutash@hotmail.com>
2025-10-29 18:58:13 -0400

    .added messages to side menu

--- src/app/side-menu/side-menu.ts
+++ src/app/side-menu/side-menu.ts
@@ -13,17 +13,11 @@ export class SideMenu {
   homeLabel: String = "";
   messagesLabel: String = "";
   detailsLabel: String = "";
-  linkList: string[] = [];
-  labelList: string[] = []
-
 
   constructor() {
-    this.homeLabel =
-    this.messagesLabel = "";
-    this.detailsLabel = "";
-
-    this.linkList=[];
-    this.labelList =[];
+    this.homeLabel = 'Home';
+    this.messagesLabel = "Go to Messages";
+    this.detailsLabel = "Go to Details";
   }
 
   linkClicked(i: number) {

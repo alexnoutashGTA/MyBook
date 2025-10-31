3a75274a451932cbc93afbe8378bc0be1f1fd379
Alex Noutash <alnoutash@hotmail.com>
2025-10-31 18:17:36 -0400

    done

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {Messages} from './messages/messages';
 
 
 @NgModule({
   imports: [RouterModule.forRoot([
-
-
+    {
+      path: 'messages',
+      component: Messages,
+    },
 
   ])],
   exports: [RouterModule]

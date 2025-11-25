06d0a06639bb49e17a8e074f9f4ab7b6f5f32661
Alex Noutash <alnoutash@hotmail.com>
2025-11-24 19:42:16 -0500

    Added Angular Form

--- src/app/messages/messages.ts
+++ src/app/messages/messages.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-messages',
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './messages.html',
   styleUrl: './messages.css',
 })
-export class Messages {
+export class Messages  {
+
+  saveMessage = (values: any) => {
+    console.log(values);
+  }
 
 }
